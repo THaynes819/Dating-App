8c7cddfa41647efd50b088e735b40abf5f449beb
mogstream <tracyehaynes@gmail.com>
2022-01-12 22:33:09 -0500

    Initial Commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; } // Important to keep the int name Id for the framework to understand it's an identifier convention
+        public string UserName { get; set; } // UserName is a convention as well
+
+    }
+}
